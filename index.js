5cd46a2c6fe266ac4568193e3a817dcb5c251ff4
EnsioS <ensiosuonpera@luukku.com>
2018-02-08 23:02:00 +0200

    teht 3.17, puhelinluettelo ja tietokanta, osa 5

--- index.js
+++ index.js
@@ -10,29 +10,6 @@ app.use(cors())
 app.use(bodyParser.json())
 app.use(morgan('tiny'))
 
-// let persons = [
-//   {
-//     name: "Arto Hellas",
-//     number: "040-123456",  
-//     id: 1,
-//   },
-//   {
-//     name: "Martti Tienari",
-//     number: "040-123456",        
-//     id: 2,
-//   },
-//   {
-//     name: "Arto Järvinen",
-//     number: "040-123456",        
-//     id: 3,
-//   },
-//   {
-//     name: "Lea Kutvonen",
-//     number: "040-123456",        
-//     id: 4,
-//   }  
-// ]
-
 app.get('/info', (request, response) => {
     const info = `<p> puhelinluettossa ${persons.length} henkilön tiedot </p>
                   <p>${new Date()}</p>`
@@ -62,16 +39,12 @@ app.get('/api/persons/:id', (request, response) => {
   }
 })
 
-//const generateId = () => Math.floor(Math.random()*1000000*persons.length)
-
 app.post('/api/persons', (request, response) => {
   const body = request.body
   
   if (body.name === undefined || body.number === undefined) {
     return response.status(400).json({ error: 'name or number missing' })
-  }// else if (persons.find(person => person.name === body.name)) {
-  //  return response.status(400).json({ error: 'name must be unique' })
-  //}
+  }
 
   const person = new Person({
     name: body.name,
@@ -88,6 +61,25 @@ app.post('/api/persons', (request, response) => {
       response.status(404).end()
     })
 })  
+
+app.put('/api/persons/:id', (request, response) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+
+  Person
+    .findByIdAndUpdate(request.params.id, person, { new: true })  
+    .then(updatedPerson => {
+      response.json(Person.format(updatedPerson))
+    })
+    .catch(error => {
+      console.log(error)
+      response.status(400).send({ error: 'malformatted id' })
+    })
+})
 
 app.delete('/api/persons/:id', (request, response) => {
   Person
